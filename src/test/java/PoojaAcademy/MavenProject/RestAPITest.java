b1dcc78451052e27184b40822983762396eff945
pooja <poojakt123@gmail.com>
2021-05-05 18:04:38 +0530

    first commit

---
+++ src/test/java/PoojaAcademy/MavenProject/RestAPITest.java
@@ -0,0 +1,18 @@
+package PoojaAcademy.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void  postJIRA()
+	{
+		System.out.println("postJIRA");
+	}
+	
+	@Test
+	public void  deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
